640354b71cd5edbef3f5d468e0b0f84f8467d20e
Munkh-Altai Chuluunbaatar <munkh-altai@MacBook-Pro.local>
2022-07-04 14:34:58 +0800

    image support micro service

--- src/modules/datagrid/elements/Image.vue
+++ src/modules/datagrid/elements/Image.vue
@@ -1,9 +1,17 @@
 <template>
-    <img :src="params.value" class="ag-grid-image"/>
+    <img :src="`${baseUrl}${params.value}`" class="ag-grid-image"/>
 </template>
 
 <script>
 import Vue from "vue";
 
-export default Vue.extend();
+
+export default Vue.extend({
+        computed: {
+            baseUrl() {
+                return this.params.baseUrl ? this.params.baseUrl : ""
+            }
+        }
+    }
+);
 </script>
