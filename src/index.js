19161f7992bbd3f2df2ab4ee7e2a6590e0993837
Tseegii Tselmeg <tseegii.it@gmail.com>
2022-09-20 19:18:26 +0800

    local changes

--- src/index.js
+++ src/index.js
@@ -11,17 +11,21 @@ import {
 } from './store/store'
 window.axios = axios;
 
-Vue.prototype.$init = window.init
-Vue.prototype.$user = window.init.user;
+if(window.init) {
+    Vue.prototype.$init = window.init
+    if(window.init.user){
+        Vue.prototype.$user = window.init.user;
+    }
+    if(window.init.project){
+        Vue.prototype.$project = window.init.project;
+        Vue.prototype.$projectSettings = window.init.projectSettings;
+    }
+}
+
 axios.interceptors.request.use(function (config) {
     config.headers['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     return config;
 });
-
-if(window.init.project){
-    Vue.prototype.$project = window.init.project;
-    Vue.prototype.$projectSettings = window.init.projectSettings;
-}
 
 new Vue({
     router,
