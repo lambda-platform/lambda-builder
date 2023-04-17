11fc0a0d719c52ead2f6ebb75dbf7870dfc5d8d5
Tseke <ts.tserenbaatar@gmail.com>
2023-04-17 15:06:52 +0800

    multi images count fix

--- src/modules/datagrid/elements/Image.vue
+++ src/modules/datagrid/elements/Image.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <div v-if="this.multiImage == true" class="multi-images">
-            <img v-for="(item, index) in this.images" v-if="index < 3" :key="item.name" :src="`${baseUrl}${item.response}`" class="ag-grid-image-multi"/>
-            <span class="image-plus" v-if="this.images.length > 3">+{{ this.plusCount }}</span>
+        <div v-if="multiImage" class="multi-images">
+            <img v-for="item in images" :key="item.name" :src="`${baseUrl}${item.response}`" class="ag-grid-image-multi"/>
+            <span class="image-plus" v-if="plusCount > 1">+{{ plusCount }}</span>
         </div>
         <div v-else>
-            <img v-if="this.params.value == null" :src="`${baseUrl}/assets/lambda/images/no-image.png`" class="ag-grid-image"/>
+            <img v-if="params.value == null" :src="`${baseUrl}/assets/lambda/images/no-image.png`" class="ag-grid-image"/>
             <img v-else :src="`${baseUrl}${params.value}`" class="ag-grid-image"/>
         </div>
     </div>
@@ -27,8 +27,12 @@ export default Vue.extend({
                     let jsonData = JSON.parse(this.params.value);
                     if (Array.isArray(jsonData)) {
                         this.multiImage = true;
-                        this.images = jsonData;
-                        this.plusCount = this.images.length - 3;
+                        if (jsonData.length <= 4) {
+                            this.images = jsonData;
+                        } else {
+                            this.images = jsonData.slice(0, 3);
+                            this.plusCount = jsonData.length - 3;
+                        }
                     } else {
                         this.multiImage = false;
                     }
