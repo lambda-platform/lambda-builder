8054d6105cb763a81cbc75a4b0d87430fe748a47
munkh-altai <munkaltai@gmail.com>
2024-07-02 09:45:50 +0800

    agent default template bug fixed

--- src/modules/agent/router.js
+++ src/modules/agent/router.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 function load(component) {
     if (window.lambda.local_agent && typeof window.lambda.local_agent !== undefined && window.lambda.local_agent !== null && window.lambda.local_agent !== '' && window.lambda.local_agent !== undefined) {
         try {
-            return require(`agent/${component}`).default;
+            return require(`./views/theme/aside/index`).default;
         } catch (err) {
             console.log('not local');
         }
