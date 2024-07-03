a615dc7f77f6f25dfbe2c657fe1c68b94c5bf0ac
Tseegii Tselmeg <tseegii.it@gmail.com>
2024-07-03 18:03:48 +0800

    Fixed local agent

--- src/modules/agent/router.js
+++ src/modules/agent/router.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 function load(component) {
     if (window.lambda.local_agent && typeof window.lambda.local_agent !== undefined && window.lambda.local_agent !== null && window.lambda.local_agent !== '' && window.lambda.local_agent !== undefined) {
         try {
-            return require(`./views/theme/aside/index`).default;
+            return require(`agent/${component}`).default
         } catch (err) {
             console.log('not local');
         }
