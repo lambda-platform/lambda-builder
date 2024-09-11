4781e193e00bbc5f5c1dd9a0aebd014dcb1dfe5c
Tseke <ts.tserenbaatar@gmail.com>
2024-09-11 22:31:43 +0800

    empty pull tseke list

--- src/modules/krud/templates/list.vue
+++ src/modules/krud/templates/list.vue
@@ -1,37 +1,37 @@
 <template>
     <section class="list-template">
         <div :class="`crud-page ${hideHeader ? 'no-header' : '' }`">
-            <!--            <div class="crud-page-header">-->
-            <!--                            <div v-if="hasNavSlot" class="krud-left">-->
-            <!--                                <div class="crud-page-header-left">-->
-            <!--                                    <slot name="nav"></slot>-->
-            <!--                                </div>-->
-            <!--                            </div>-->
+<!--            <div class="crud-page-header">-->
+<!--                <div v-if="hasNavSlot" class="krud-left">-->
+<!--                    <div class="crud-page-header-left">-->
+<!--                        <slot name="nav"></slot>-->
+<!--                    </div>-->
+<!--                </div>-->
 
-            <!--                <div v-else class="crud-page-header-left">-->
-            <!--                    <i v-if="$props.icon" :class="icon"></i>-->
-            <!--                    <h3 v-if="$props.title">{{ $props.title.replace(/-/g, ' ') }}</h3>-->
-            <!--                    <slot name="nav"></slot>-->
-            <!--                </div>-->
+<!--                <div v-else class="crud-page-header-left">-->
+<!--                    <i v-if="$props.icon" :class="icon"></i>-->
+<!--                    <h3 v-if="$props.title">{{ $props.title.replace(/-/g, ' ') }}</h3>-->
+<!--                    <slot name="nav"></slot>-->
+<!--                </div>-->
 
-            <!--                <div class="crud-page-header-right">-->
-            <!--                    <div class="tool-options">-->
-            <!--                        <slot name="tooloptions"></slot>-->
-            <!--                    </div>-->
-            <!--                    <krudtools :search="search"-->
-            <!--                               :refresh="refresh"-->
-            <!--                               :exportExcel="exportExcel"-->
-            <!--                               :print="print"-->
-            <!--                               :save="save"-->
-            <!--                               :isPrint="isPrint"-->
-            <!--                               :isExcel="isExcel"-->
-            <!--                               :isRefresh="isRefresh"-->
-            <!--                               :isSave="isSave"-->
-            <!--                               :isSearch="isSearch"-->
-            <!--                    />-->
-            <!--                    <slot name="right"></slot>-->
-            <!--                </div>-->
-            <!--            </div>-->
+<!--                <div class="crud-page-header-right">-->
+<!--                    <div class="tool-options">-->
+<!--                        <slot name="tooloptions"></slot>-->
+<!--                    </div>-->
+<!--                    <krudtools :search="search"-->
+<!--                               :refresh="refresh"-->
+<!--                               :exportExcel="exportExcel"-->
+<!--                               :print="print"-->
+<!--                               :save="save"-->
+<!--                               :isPrint="isPrint"-->
+<!--                               :isExcel="isExcel"-->
+<!--                               :isRefresh="isRefresh"-->
+<!--                               :isSave="isSave"-->
+<!--                               :isSearch="isSearch"-->
+<!--                    />-->
+<!--                    <slot name="right"></slot>-->
+<!--                </div>-->
+<!--            </div>-->
 
             <krud-header-bs v-if="$theme == 'bs'"
                             :title="title"
@@ -129,5 +129,10 @@ export default {
         "krud-header-bs": krudHeaderBs
     },
     mixins: [mixins],
+    created() {
+        console.log('here');
+        console.log($theme);
+        console.log(this);
+    }
 };
 </script>
